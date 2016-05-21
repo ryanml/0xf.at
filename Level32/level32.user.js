ec127de7cb86a3c45a6a17f62c342a61f6e89f3b
ryanml <rlanese@asu.edu>
2016-05-21 19:08:14 -0400

    Refactoring solution

--- Level32/level32.user.js
+++ Level32/level32.user.js
@@ -1,76 +1,64 @@
 // ==UserScript==
-// @name        Level32
-// @namespace   http://github.com/ryanml
-// @description Solves and Submits level 32 of 0x.f4t
-// @include     https://0xf.at/play/32
+// @name        Level 32 
+// @namespace   https://github.com/ryanml
+// @description Solves and submits level 32 of 0xf.at
+// @include     https://0xf.at/play/32*
 // @version     1
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
 (function () {
-  // Declare our password and misspelled word array
-  var password = '';
-  wordList = [
-  ];
-  //Uses cross site request to retrieve the dictionary text file.
-  var list = GM_xmlhttpRequest({
+  // Get the dom elements we need to work with
+  var wordBlock = document.getElementsByTagName('blockquote') [0].innerHTML;
+  var passwordField = document.getElementById('pw');
+  var submit = document.getElementsByTagName('input') [1];
+  // Request the dictionary file
+  var dictFile = GM_xmlhttpRequest({
     method: 'GET',
     url: 'http://rlanesedev.iqlaunch.com/latindictionary.txt',
     synchronous: true
   });
-  // Break up response by new lines
-  var rawDictionary = list.responseText.split('\n');
-  // Trim whitespace from elements
-  var dictionary = rawDictionary.map(function (word) {
-    return word.trim();
-  });
-  // Get what we'll need from the DOM, the wordlist and the password field.
-  var blockText = document.getElementsByTagName('blockquote') [0].innerHTML;
-  var passInput = document.getElementById('pw');
-  var passSubmit = document.getElementsByTagName('input') [1];
-  // Split the random list words by whitespace and put them in an array
-  var rawWords = blockText.split(' ');
-  // Force all words to lower case, trims extra whitespace and periods.
-  var wordArray = rawWords.map(function (word) {
-    return word.toLowerCase().trim().replace('.', '');
-  });
-  //Fill the wordlist with the incorrect word pairs
-  getIncorrectWords(dictionary, wordArray);
-  // Look through our wordlist, check to make sure the missing letter is just a one char val difference, if not, we ignor it. 
-  for (var i = 0; i < wordList.length; i++) {
-    var correct = wordList[i][0];
-    var wrong = wordList[i][1];
-    for (var j = 0; j < correct.length; j++) {
-      if (correct.charCodeAt(j) == wrong.charCodeAt(j) - 1) {
-        password += correct.charAt(j);
-      }
+  // Put in dictionary list
+  var dictionary = dictFile.responseText.split('\n').map(word => word.trim());
+  // Create a clean list of the given words
+  var words = wordBlock.split(' ').map(word => 
+    word.replace('.', '').trim().toLowerCase()
+  );
+  // Get all of the misspelled words
+  var misspelledWords = [];
+  for (var i = 0; i < words.length; i++) {
+    if (dictionary.indexOf(words[i]) < 0) {
+      misspelledWords.push(words[i]);
     }
   }
-  // When that's finished, we fill in the password and submit the form.
-  passInput.value = password;
-  passSubmit.click();
-  // Gets all of the misspelled words in the block
-  function getIncorrectWords(dict, words) {
-    for (var i = 0; i < words.length; i++) {
-      if (dict.indexOf(words[i]) == - 1) {
-        getMatches(dict, words[i]);
-      }
-    }
-  }
-  // Finds all of the potential correct matches for our misspelled words.
-  function getMatches(dict, word) {
-    for (var i = 0; i < dict.length; i++) {
-      if (dict[i].length == word.length) {
-        var wc = 0;
-        for (var j = 0; j < dict[i].length; j++) {
-          if (dict[i][j] != word[j]) {
-            wc++;
+  // Set up solution
+  var solution = misspelledWords.map((word) => {
+    // For all the misspelled words, go through the dictionary
+    for (var i = 0; i < dictionary.length; i++) {
+      // Ignore the word if it isn't the same length
+      if (dictionary[i].length == word.length) {
+        var mc = 0; 
+        var m = [];
+        // Go through both words by chars
+        for (var k = 0; k < word.length; k++) {
+          // If there is a difference, 
+          if (dictionary[i][k] != word[k]) {
+            // Update difference count
+            mc++;
+            // Push the two characters on the array
+            m.push([dictionary[i][k], word[k]]);
           }
         }
-        if (wc == 1) {
-          wordList.push([dict[i],
-          word]);
+        // If there's only one difference and that difference is forward one char, append it to solution
+        if (mc == 1) {
+          if (m[0][1].charCodeAt() == m[0][0].charCodeAt() + 1) {
+            return m[0][0];
+          }
         }
       }
     }
-  }
+  }).join('');
+  // Fill in the password field with solution
+  passwordField.value = solution;
+  // Submit the solution
+  submit.click();
 }) ();
